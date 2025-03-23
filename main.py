03871ba9ee714084b217707f6dab44ad74edfd81
omwitsa-etole <omwitsaetole@gmail.com>
2025-03-23 11:48:17 +0300

    stl

--- main.py
+++ main.py
@@ -629,4 +629,4 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8081))#host="0.0.0.0",
-    app.run( port=port,debug=True )
+    app.run( debug=False )
