fda62f210803bfa81fcce99bc71a2a691127d8f5
omwitsa-etole <omwitsaetole@gmail.com>
2025-03-29 18:19:44 +0300

    add gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,4 +1,4 @@
-bind = '0.0.0.0:5000'  # Bind to 0.0.0.0 on port 8000
+bind = '127.0.0.1:5000'  # Bind to 0.0.0.0 on port 8000
 workers = 3  # Number of worker processes
 threads = 2  # Number of threads per worker
 timeout = 30  # Timeout in seconds for each worker
